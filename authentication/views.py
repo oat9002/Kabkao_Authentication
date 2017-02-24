e04aa8be927c242c6732981fc26b19540ec8c5c7
Unknown <moobinkung@windowslive.com>
2017-02-24 17:33:47 +0700

    - added an ErrorCode module for holding any error codes.
    - added a full user serializer and a basic user serializer to seperate a level of permission for accessing the user information.
    - added an api for login and signup.
    - added a ResponseFormat module for keeping a json standard in data response, and to prevent a confusion of response format.
    - added a template for login and signup.

--- authentication/views.py
+++ authentication/views.py
@@ -1,25 +1,74 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
 from django.http import HttpResponse
-import pyrebase
+# import pyrebase
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-config = {
-    'apiKey': "AIzaSyAGPTJC14nFrWJE0e7348YftzcYkUkEhG0",
-    'authDomain': "kabkao-fc139.firebaseapp.com",
-    'databaseURL': "https://kabkao-fc139.firebaseio.com",
-    'storageBucket': "kabkao-fc139.appspot.com",
-    'messagingSenderId': "279221787005"
-}
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
+import ErrorCode
+import ResponseFormat
+from authentication.permissions import IsAdmin
 
-def authentication(request):
-    user = auth.sign_in_with_email_and_password(request.GET['email'], request.GET['password'])
-    return HttpResponse(user['idToken'])
+# config = {
+#     'apiKey': "AIzaSyAGPTJC14nFrWJE0e7348YftzcYkUkEhG0",
+#     'authDomain': "kabkao-fc139.firebaseapp.com",
+#     'databaseURL': "https://kabkao-fc139.firebaseio.com",
+#     'storageBucket': "kabkao-fc139.appspot.com",
+#     'messagingSenderId': "279221787005"
+# }
+# firebase = pyrebase.initialize_app(config)
+# auth = firebase.auth()
+#
+# def authentication(request):
+#     user = auth.sign_in_with_email_and_password(request.GET['email'], request.GET['password'])
+#     return HttpResponse(user['idToken'])
 
-def verify_account(request):
-    try:
-        verification = auth.get_account_info(request.GET['idToken'])
-    except:
-        return HttpResponse('not verify')
-    return HttpResponse('verify')
+# def verify_account(request):
+#     try:
+#         verification = auth.get_account_info(request.GET['idToken'])
+#     except:
+#         return HttpResponse('not verify')
+#     return HttpResponse('verify')
 # Create your views here.
+from authentication.serializer import FullAccountSerializer
+
+
+class SignUp(APIView):
+
+    @staticmethod
+    def get(request):
+        return render(request, 'sign_up.html')
+
+    @staticmethod
+    def post(request):
+        try:
+            if User.objects.filter(email=request.data['email']).exists():
+                raise Exception
+            user = User.objects.create_user(request.data['username'], request.data['email'], request.data['password'])
+            Token.objects.create(user=user)
+            return Response({'success': True,
+                         'payload': {
+                             'token': Token.objects.get(user=user).key,
+                             'user_data': FullAccountSerializer(user).data
+                         }})
+        except Exception as ex:
+            return Response(ResponseFormat.error(ErrorCode.USERNAME_OR_EMAIL_ALREADY_EXISTS, "Username or email may already exist. Please use another one."))
+
+
+class Authentication(APIView):
+
+    @staticmethod
+    def get(request):
+        return render(request, 'login.html')
+
+    @staticmethod
+    def post(request):
+        user = authenticate(username=request.data['username'], password=request.data['password'])
+        if user:
+            return Response(ResponseFormat.success({
+                                 'token': Token.objects.get(user=user).key,
+                                 'user_data': FullAccountSerializer(user).data
+                             }))
+        return Response(ResponseFormat.error(ErrorCode.USERNAME_OR_PASSWORD_INCORRECT, "Username or password is incorrect. Please try again."))
